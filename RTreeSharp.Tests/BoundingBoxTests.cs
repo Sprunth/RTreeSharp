212cef83ca6897ebc6dcedb17ddc7be6556d963d
Dylan <sprunth@gmail.com>
2019-10-09 00:49:06 -0700

    add tests

---
+++ RTreeSharp.Tests/BoundingBoxTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using RTreeSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RTreeSharp.Tests
+{
+    public class BoundingBoxTests
+    {
+        [Fact()]
+        public void EnlargedBoundingBoxTest()
+        {
+            var baseBounds = new BoundingBox(0, 0, 10, 10);
+            var newBounds = new BoundingBox(10, 10, 5, 15);
+
+            var enlargedBounds = BoundingBox.EnlargedBoundingBox(baseBounds, newBounds);
+            Assert.Equal(0, enlargedBounds.Left);
+            Assert.Equal(0, enlargedBounds.Top);
+            Assert.Equal(15, enlargedBounds.Right);
+            Assert.Equal(25, enlargedBounds.Bottom);
+        }
+
+        [Fact()]
+        public void AreaTest()
+        {
+            var baseBounds = new BoundingBox(5, 5, 10, 20);
+            Assert.Equal(baseBounds.Area, 10 * 20);
+        }
+    }
+}
